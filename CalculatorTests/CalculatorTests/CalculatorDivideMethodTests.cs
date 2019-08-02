95400d4b0cf089c9db71388ef12d8c9c2e0c3c83
Dzmitry Dubovick <dzmitry_dubovick@epam.com>
2019-08-02 16:22:46 +0300

    add tests for divide method

---
+++ CalculatorTests/CalculatorTests/CalculatorDivideMethodTests.cs
@@ -0,0 +1,32 @@
+﻿using CSharpCalculator;
+using NUnit.Framework;
+using System;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    class CalculatorDivideMethodTests
+    {
+        private Calculator calculator;
+        private Random rand;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+            rand = new Random();
+        }
+
+        [TestCase(4, 2, 2)]
+        [TestCase(4, -1, -4)]
+        [TestCase(0, 5, 0)]
+        [TestCase(1, 4, 0.25)]
+        [TestCase(1, 0, 0.25)]
+
+        public void DivideByNoNullDenominatorTest(double numerator, double denominator, double expected)
+        {
+            var actualValue = calculator.Divide(numerator, denominator);
+            Assert.AreEqual(expected, actualValue);
+        }
+    }
+}
